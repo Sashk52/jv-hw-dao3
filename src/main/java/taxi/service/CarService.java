09ef5eb105c9859aa1fcab85fe762bb46939c23c
Sashk52 <a.tsven@gmail.com>
2021-01-21 11:15:33 +0200

    solution new

--- src/main/java/taxi/service/CarService.java
+++ src/main/java/taxi/service/CarService.java
@@ -1,5 +1,6 @@
 package taxi.service;
 
+import java.util.List;
 import taxi.model.Car;
 import taxi.model.Driver;
 
@@ -8,4 +9,6 @@ public interface CarService extends GenericService<Car, Long> {
     void addDriverToCar(Driver driver, Car car);
 
     void removeDriverFromCar(Driver driver, Car car);
+
+    List<Car> getAllByDriver(Long driverId);
 }
