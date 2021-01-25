7fc095889266398ed58ff6a070330fbc0293bde1
Sashk52 <a.tsven@gmail.com>
2021-01-26 00:47:00 +0200

    made some correction

--- src/main/java/taxi/controller/GetMyCurrentCarsController.java
+++ src/main/java/taxi/controller/GetMyCurrentCarsController.java
@@ -1,16 +1,15 @@
 package taxi.controller;
 
-import taxi.lib.Injector;
-import taxi.model.Car;
-import taxi.service.CarService;
-
+import java.io.IOException;
+import java.util.List;
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import javax.servlet.http.HttpSession;
-import java.io.IOException;
-import java.util.List;
+import taxi.lib.Injector;
+import taxi.model.Car;
+import taxi.service.CarService;
 
 public class GetMyCurrentCarsController extends HttpServlet {
     private static final Injector injector = Injector.getInstance("taxi");
@@ -21,8 +20,8 @@ public class GetMyCurrentCarsController extends HttpServlet {
             throws ServletException, IOException {
 
         HttpSession session = req.getSession();
-        Long driver_id = (Long) session.getAttribute("drivers_id");
-        List<Car> allCarsByDriver = this.carService.getAllByDriver(driver_id);
+        Long driverId = (Long) session.getAttribute("drivers_id");
+        List<Car> allCarsByDriver = this.carService.getAllByDriver(driverId);
 
         req.setAttribute("carsByDriver", allCarsByDriver);
         req.getRequestDispatcher("/WEB-INF/views/cars/driver/all.jsp").forward(req, resp);
