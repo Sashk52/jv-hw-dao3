99b4e166b61dd1389749cb5fdda9f1133daee6f9
Sashk52 <a.tsven@gmail.com>
2021-01-25 15:35:27 +0200

    made solution

--- src/main/java/taxi/controller/GetAllDriversController.java
+++ src/main/java/taxi/controller/GetAllDriversController.java
@@ -13,13 +13,10 @@ import taxi.service.DriverService;
 public class GetAllDriversController extends HttpServlet {
     private static final Injector injector = Injector.getInstance("taxi");
     private DriverService driverService = (DriverService) injector.getInstance(DriverService.class);
-
     @Override
     protected void doGet(HttpServletRequest req, HttpServletResponse resp)
             throws ServletException, IOException {
-
         List<Driver> allDrivers = this.driverService.getAll();
-
         req.setAttribute("drivers", allDrivers);
         req.getRequestDispatcher("/WEB-INF/views/drivers/all.jsp").forward(req, resp);
     }
